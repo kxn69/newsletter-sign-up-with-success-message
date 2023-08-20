43eff9a8140a382278f31183b0e3c7c7704cc413
Kunmi A <113647454+kxn69@users.noreply.github.com>
2023-08-20 03:30:10 +0100

    added js

---
+++ dist/newsletter.js
@@ -0,0 +1,49 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+const formContainer = document.querySelector('.form');
+const successMessage = document.querySelector('.success-message')
+const form = document.querySelector('form');
+const warnText = form.querySelector('.error-msg');
+const submitButton = form.querySelector('#submit-btn');
+const confirmationMsg = document.querySelector('.confirmation');
+const dismissBtn = document.querySelector('.dismiss-btn');
+
+function handleInput(e) {
+  const input = e.target;
+  console.log(input)
+  const userEmail = e.target.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!(userEmail.length >= 1)) {
+    console.log('ddd')
+    input.classList.remove('warn');
+    submitButton.disabled = true;
+  } else if (!emailRegex.test(userEmail)) {
+    input.classList.add('warn');
+    warnText.hidden = false;
+    submitButton.disabled = true;
+  } else {
+    input.classList.remove('warn');
+    warnText.hidden = true;
+    submitButton.disabled = false;
+  }
+}
+
+function handleForm(e) {
+  e.preventDefault();
+  const email = e.target.querySelector('#email').value;
+
+  confirmationMsg.innerHTML = `A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription.`;
+  formContainer.style.display = 'none';
+  successMessage.hidden = false;
+}
+
+function handleDismissBtn(e) {
+  console.log(e.target)
+  successMessage.hidden = true;
+  formContainer.style.display = 'grid';
+}
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleForm);
+dismissBtn.addEventListener('click', handleDismissBtn);
